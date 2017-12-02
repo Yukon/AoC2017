5b9b80bc74e324bc06874c7c15059a58928ca2f7
Yukon Vinecki <yukonvinecki@gmail.com>
2017-12-01 16:03:32 -0800

    Day one complete

---
+++ Day1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	fmt.Printf("Enter captcha: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	captcha := make([]int, len(input)-1)
+	for i, elm := range input[:len(input)-1] {
+		str := string(elm)
+		num, _ := strconv.ParseInt(str, 10, 8)
+		captcha[i] = int(num)
+	}
+
+	result := 0
+	for i, elm := range captcha {
+		if elm == captcha[(i+1)%len(captcha)] {
+			result += elm
+		}
+	}
+
+	fmt.Printf("%d\n", result)
+}
